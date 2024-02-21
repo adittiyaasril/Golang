a6d0888115be65941cdd6d107e5b551e658845cd
adittiyaasril <adittiyaasril369@gmail.com>
2024-02-21 15:29:06 +0700

    feat: session 1

---
+++ Sesi 1/project-pertama/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+		//Println for new line print
+    // fmt.Println("Hello, World!")
+		// fmt.Println("Adittiya", "Jordan")
+
+		//Print for same line print
+		// fmt.Print("Hello")
+		// fmt.Print("World")
+
+		var name string = "Adittiya"
+		var age int = 2000
+
+		fmt.Println("This is my name ==> ", name)
+		fmt.Println("This is my age ==> ", age)
+}
